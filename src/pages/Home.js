ffdc8e7772adf2b513d74a3e2deebe1d16f5bef2
roaayala <roaayala.ahm@gmail.com>
2026-04-22 21:29:29 +0800

    show basic content of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 export class Home {
-	constructor() {}
+	constructor() {
+		this.contents = {
+			image: '',
+			headline: 'The Soul of Indoensian Spice',
+			body: `Welcome to Bumbu Heritage. We bring the rich, complex, and vibrant flavors of the Indonesian archipelago straight to your plate. From the fiery sambals of Sumatra to the sweet, aromatic soy-glazed dishes of Java, our menu is a celebration of deep-rooted tradition and modern culinary elegance. Forget the generic fast food; sit down, relax, and let our centuries-old spice recipes tell you a story.`,
+		};
+	}
 
 	render() {
-		return `<h1>Home</h1>`;
+		return `<div>
+		<h1>${this.contents.headline}</h1>
+		<p>${this.contents.body}</p>
+		</div>`;
 	}
 }
