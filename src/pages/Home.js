31e5b1d38b42f409d5093375c823b71e510d4f1e
roaayala <roaayala.ahm@gmail.com>
2026-04-22 21:39:46 +0800

    show basic content of menu page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 export class Home {
 	constructor() {
-		this.contents = {
+		this.content = {
 			image: '',
 			headline: 'The Soul of Indoensian Spice',
 			body: `Welcome to Bumbu Heritage. We bring the rich, complex, and vibrant flavors of the Indonesian archipelago straight to your plate. From the fiery sambals of Sumatra to the sweet, aromatic soy-glazed dishes of Java, our menu is a celebration of deep-rooted tradition and modern culinary elegance. Forget the generic fast food; sit down, relax, and let our centuries-old spice recipes tell you a story.`,
@@ -9,8 +9,8 @@ export class Home {
 
 	render() {
 		return `<div>
-		<h1>${this.contents.headline}</h1>
-		<p>${this.contents.body}</p>
+		<h1>${this.content.headline}</h1>
+		<p>${this.content.body}</p>
 		</div>`;
 	}
 }
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,7 +1,44 @@
 export class Menu {
-	constructor() {}
+	constructor() {
+		this.content = {
+			image: '',
+			headline: 'Taste The Archipelago',
+			menus: [
+				{
+					name: 'Signature Beef Rendang',
+					description:
+						'The undisputed king of curries. Slow-cooked, caramelized beef simmered for 8 hours in rich coconut milk and our secret blend of 15 native spices. Tender, complex, and unforgettable.',
+					price: 125_000,
+				},
+				{
+					name: 'Madura Chicken Satay',
+					description:
+						'Perfectly char-grilled chicken skewers marinated in sweet soy sauce. Served generously with our signature crushed peanut sauce, a sprinkle of crispy shallots, and fresh lime.',
+					price: 99_000,
+				},
+				{
+					name: 'The Ultimate Nasi Goreng',
+					description: `Indonesia's ultimate comfort food. Wok-tossed fried rice infused with a hint of aromatic shrimp paste, topped with a crispy sunny-side-up egg, chicken chunks, and served with traditional prawn crackers.`,
+					price: 65_000,
+				},
+				{
+					name: 'Es Cendol Deligt',
+					description: `Cool down your palate with this traditional sweet treat. Chewy pandan jelly droplets bathed in fresh, creamy coconut milk and rich, melted palm sugar syrup.`,
+					price: 49_000,
+				},
+			],
+		};
+	}
 
 	render() {
-		return `<h1>Menu</h1>`;
+		return `<div>
+		<h1>${this.content.headline}</h1>
+	${this.content.menus.map((menu) => {
+		return `<div>
+		<h2>${menu.name} (Rp. ${menu.price})</h2>
+		<p>${menu.description}</p>
+		</div>`;
+	})}
+		</div>`;
 	}
 }
